f3fa09d801f30112488a42bc197b977e5195faa9
Marshal Horn <kamocat@gmail.com>
2019-04-19 15:35:50 -0700

    Added delay to give CPU time for other tasks

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 import temperature
 import schedule
 import datetime
+import time
 from gpiozero import LED
 
 s = temperature.init()
 furnace = LED(4)
 hysteresis = 0.5
+desired = 0
+actual = 100
 
 while 1:
-	print(datetime.datetime.now())
-	actual = temperature.read(s)
-	print("current temp is", actual)
-	desired = schedule.get()
 	if (actual - desired) > hysteresis:
 		# Turn off furnace
 		# Note: This may result in over-heating, since the
@@ -21,9 +20,16 @@ while 1:
 		# the blower on for a few minutes to cool down
 		furnace.off()
 		print("Turning furnace off...")
-
 	elif (desired - actual) > hysteresis:
 		# Turn on furnace
 		furnace.on()
 		print("Turning furnace on...")
+	# Sleep for several seconds
+	time.sleep(3)
+	actual = temperature.read(s)
+	print(datetime.datetime.now().time(), "current temp is", actual)
+	try:
+		desired = schedule.get()
+	except:
+		print("Google Calendar timed out.")
 	
