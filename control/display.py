d76ce549974173527c053991f9823af2aa0d24d1
Marshal Horn <kamocat@gmail.com>
2019-05-04 10:24:38 -0700

    Display works, but needs sudo to run.

--- control/display.py
+++ control/display.py
@@ -1,9 +1,13 @@
 import pygame
 from datetime import datetime
+import os
+
+os.putenv('SDL_FBDEV', '/dev/fb1')
 
 pygame.init()
 size = [320,240]
 screen = pygame.display.set_mode(size)
+pygame.mouse.set_visible(False)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255, 0, 0)
@@ -44,18 +48,4 @@ def weather():
 	pygame.display.update(weather_rect)
 
 
-update('50', '60')
-
-# Loop until the user clicks the close button
-done = False
-clock = pygame.time.Clock()
-while not done:
-	# This limits the while loop to a max of 10 times per second.
-	# Leave this out and we will use all CPU we can.
-	clock.tick(10)
-	 
-	for event in pygame.event.get(): # User did something
-		if event.type == pygame.QUIT: # If user clicked close
-			done=True # Flag that we are done so we exit this loop
-
-pygame.quit()
+#update('50', '60')
